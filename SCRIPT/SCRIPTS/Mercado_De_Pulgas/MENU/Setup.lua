77df0abc2ca0376836c266f4a7a1dd9fc6d1ad5c
sebasparra26 <sebas-parra-20@hotmail.com>
2025-04-28 20:40:57 -0500

    submit

---
+++ SCRIPT/SCRIPTS/Mercado_De_Pulgas/MENU/Setup.lua
@@ -0,0 +1,16 @@
+configuracionEntregaB = {
+    origen = { x = 156125, y = 0, z = -100878 },
+    velocidad = 180
+}
+configuracionEntregaR = {
+    origen = { x = -72828, y = 0, z = 201007 },
+    velocidad = 180
+}
+MercadoSetuptimerR = {
+    Total = 99999,
+    Intervalo = 99999
+}
+MercadoSetuptimerB = {
+    Total = 99999,
+    Intervalo = 99999
+}
